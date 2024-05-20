3fcdc3a238c6fad4d03c04680fda1ad661dea0d1
ludmila davletshina <ludaplv@mail.ru>
2024-05-20 20:37:30 +0300

    checked linters

--- src/processing.py
+++ src/processing.py
@@ -1,38 +1,22 @@
 from typing import Iterable
 
 
-# def get_dict(list: str, value: Iterable[str] = "EXECUTED") -> str:
-#     """Функция возвращает новый список, содержащий только те словари, у которых ключ state
-#     содержит переданное в функцию значение"""
-#     dict_name = [
-#         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-#         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-#         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-#         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-#     ]
-#     new_dict = []
-#     for i in dict_name:
-#         if i["state"] == value:
-#             new_dict.append(i)
-#     return new_dict
+def get_dict(list: str, value: Iterable[str] = "EXECUTED") -> str:
+    """Функция возвращает новый список, содержащий только те словари, у которых ключ state
+    содержит переданное в функцию значение"""
+    dict_name = [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+    new_dict = []
+    for i in dict_name:
+        if i["state"] == value:
+            new_dict.append(i)
+    return new_dict
 
-#Напишите функцию, которая принимает на вход список словарей и возвращает новый список,
-# в котором исходные словари отсортированы по убыванию даты (ключ date).
-# Функция принимает два аргумента, второй необязательный задает порядок сортировки (убывание, возрастание).
-# Вход функции
-[{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
- {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
- {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
- {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
-# Выход функции (сортировка по убыванию, т. е. сначала самые последние операции)
-# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-#  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-#  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-#  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
 
 def get_sort_dict(list: str, ascending=True) -> str:
     """Функция сортирует по убыванию даты. Второй аргумент необязательный, задает порядок сортировки"""
-    return sorted(list, key=lambda x: x.get('date', 0), reverse=ascending)
-
-
+    return sorted(list, key=lambda x: x.get("date", 0), reverse=ascending)
