75a9717cc036251e48496ea0a89cd9efc149192b
ludmila davletshina <ludaplv@mail.ru>
2024-05-20 15:26:09 +0300

    update main.py

--- src/main.py
+++ src/main.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     number = input()
     print(name_card(number))
 
-    # print(datetime_str("2018-07-11T02:26:18.671407"))
+    print(datetime_str("2018-07-11T02:26:18.671407"))
