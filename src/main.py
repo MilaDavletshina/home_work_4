ebb2d9af3fe7939d352167ff6e73832d6261b16a
ludmila davletshina <ludaplv@mail.ru>
2024-05-21 14:15:27 +0300

    внесены изменения в git_dict согласно замечанию

--- src/main.py
+++ src/main.py
@@ -1,8 +1,44 @@
 from widget import datetime_str, name_card
+from processing import get_dict, get_sort_dict
+
 
 if __name__ == "__main__":
     number = input()
     print(name_card(number))
 
+
     print(datetime_str("2018-07-11T02:26:18.671407"))
+
+
+    list = [{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}]
+    print(get_dict(list, ))
+
+
+
+print(
+    get_sort_dict(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {
+                "id": 939719570,
+                "state": "EXECUTED",
+                "date": "2018-06-30T02:08:58.425572",
+            },
+            {
+                "id": 594226727,
+                "state": "CANCELED",
+                "date": "2018-09-12T21:27:25.241689",
+            },
+            {
+                "id": 615064591,
+                "state": "CANCELED",
+                "date": "2018-10-14T08:21:33.419441",
+            },
+        ]
+    ),
+)
+
 
