b77b5a44d6fe2b7e2aeaa309931165d4ff70ac71
ludmila davletshina <ludaplv@mail.ru>
2024-05-23 10:02:41 +0300

    папка tests проверена на black

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,10 +1,9 @@
-
 from src.masks import card_mask, account_mask
 
 
 def test_card_mask():
-    assert card_mask('7000792289606361') == '7000 79** **** 6361'
+    assert card_mask("7000792289606361") == "7000 79** **** 6361"
+
 
 def test_account_mask():
-    assert account_mask('73654108430135874305') == '**4305'
-
+    assert account_mask("73654108430135874305") == "**4305"
