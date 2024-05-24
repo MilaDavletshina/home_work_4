c7ac03122f4437c77db353c223b4faf0dcddc6c2
ludmila davletshina <ludaplv@mail.ru>
2024-05-24 11:01:05 +0300

    название словаря изменено, изменен тип аннотации

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 from typing import Iterable
-from typing import Any
 
-def get_dict(name_dict: Any, value: Iterable[str] = "EXECUTED") -> str:
+
+def get_dict(name_dict: Iterable, value: Iterable[str] = "EXECUTED") -> str:
     """Функция возвращает новый список, содержащий только те словари, у которых ключ state
     содержит переданное в функцию значение"""
     new_dict = []
@@ -11,6 +11,6 @@ def get_dict(name_dict: Any, value: Iterable[str] = "EXECUTED") -> str:
     return new_dict
 
 
-def get_sort_dict(name_dict: Any, ascending=True) -> str:
+def get_sort_dict(name_dict: Iterable, ascending=True) -> str:
     """Функция сортирует по убыванию даты. Второй аргумент необязательный, задает порядок сортировки"""
     return sorted(name_dict, key=lambda x: x.get("date", 0), reverse=ascending)
