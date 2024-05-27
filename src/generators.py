f587f1059a10fb2b650f21027ebd0652c0ea6f59
ludmila davletshina <ludaplv@mail.ru>
2024-05-27 11:01:37 +0300

    добавлен transaction.txt

--- src/generators.py
+++ src/generators.py
@@ -2,6 +2,10 @@
 def filter_by_currency(transactions, currency):
     """Функция принимает список словарей с банковскими операциями и возвращает итератор, который выдает по очереди операции, в которых указана заданная валюта"""
     with open("transactions.txt") as file:
-    for transaction in transactions:
-        if transaction['operationAmount']['currency']['code'] == currency:
-            yield transaction
+        for transaction in transactions:
+            if transaction['operationAmount']['currency']['code'] == currency:
+                yield transaction
+usd_transactions = filter_by_currency(transactions, "USD")
+
+for _ in range(2):
+    print(next(usd_transactions)["id"])
--- src/main.py
+++ src/main.py
@@ -1,48 +1,53 @@
 from src.widget import datetime_str, name_card
 from src.processing import get_dict, get_sort_dict
-
+from src.generators import filter_by_currency
 
 if __name__ == "__main__":
-    number = input()
-    print(name_card(number))
+    # number = input()
+    # print(name_card(number))
+    #
+    # print(datetime_str("2018-07-11T02:26:18.671407"))
+    #
+    # name_dict = [
+    #     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    #     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    #     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    #     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    # ]
+    # print(
+    #     get_dict(
+    #         name_dict,
+    #     )
+    # )
+    #
+    # print(
+    #     get_sort_dict(
+    #         [
+    #             {
+    #                 "id": 41428829,
+    #                 "state": "EXECUTED",
+    #                 "date": "2019-07-03T18:35:29.512364",
+    #             },
+    #             {
+    #                 "id": 939719570,
+    #                 "state": "EXECUTED",
+    #                 "date": "2018-06-30T02:08:58.425572",
+    #             },
+    #             {
+    #                 "id": 594226727,
+    #                 "state": "CANCELED",
+    #                 "date": "2018-09-12T21:27:25.241689",
+    #             },
+    #             {
+    #                 "id": 615064591,
+    #                 "state": "CANCELED",
+    #                 "date": "2018-10-14T08:21:33.419441",
+    #             },
+    #         ]
+    #     ),
+    # )
 
-    print(datetime_str("2018-07-11T02:26:18.671407"))
+    usd_transactions = filter_by_currency(transactions, "USD")
 
-    name_dict = [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    ]
-    print(
-        get_dict(
-            name_dict,
-        )
-    )
-
-    print(
-        get_sort_dict(
-            [
-                {
-                    "id": 41428829,
-                    "state": "EXECUTED",
-                    "date": "2019-07-03T18:35:29.512364",
-                },
-                {
-                    "id": 939719570,
-                    "state": "EXECUTED",
-                    "date": "2018-06-30T02:08:58.425572",
-                },
-                {
-                    "id": 594226727,
-                    "state": "CANCELED",
-                    "date": "2018-09-12T21:27:25.241689",
-                },
-                {
-                    "id": 615064591,
-                    "state": "CANCELED",
-                    "date": "2018-10-14T08:21:33.419441",
-                },
-            ]
-        ),
-    )
+    for _ in range(2):
+        print(next(usd_transactions)["id"])
