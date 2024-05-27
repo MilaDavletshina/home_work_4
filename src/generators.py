a4b5a6b70f91408a02e03a0945561c7e01f7a64e
ludmila davletshina <ludaplv@mail.ru>
2024-05-27 10:56:04 +0300

    создана функция filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -1,18 +1,7 @@
-# функция, которая принимает список словарей с банковскими операциями
-# (или объект-генератор, который выдает по одной банковской операции)
-# и возвращает итератор, который выдает по очереди операции, в которых указана заданная валюта.
-#
-# Пример вызова функции:
-#
-def filter_by_currency(transactions, "USD"):
+
+def filter_by_currency(transactions, currency):
+    """Функция принимает список словарей с банковскими операциями и возвращает итератор, который выдает по очереди операции, в которых указана заданная валюта"""
     with open("transactions.txt") as file:
-
-
-usd_transactions = filter_by_currency(transactions, "USD")
-
-for _ in range(2):
-    print(next(usd_transactions)["id"])
-# Пример вывода:
-#
-# 939719570
-# 142264268
+    for transaction in transactions:
+        if transaction['operationAmount']['currency']['code'] == currency:
+            yield transaction
