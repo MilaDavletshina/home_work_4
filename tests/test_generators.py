4dd046ff950f707a35a18a6e38c17dde4b2b8683
ludmila davletshina <ludaplv@mail.ru>
2024-05-29 15:12:05 +0300

    проверен на линтеры

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,15 +1,19 @@
-from src.generators import (card_number_generator, filter_by_currency,
-                            transaction_descriptions)
+from src.generators import (
+    card_number_generator,
+    filter_by_currency,
+    transaction_descriptions,
+)
 import pytest
 
-def test_filter_by_currency(operations):
+
+def test_filter_by_currency_usd(operations):
     usd_currency = filter_by_currency(operations, "USD")
     assert next(usd_currency)["id"] == 939719570
     assert next(usd_currency)["id"] == 142264268
     assert next(usd_currency)["id"] == 895315941
 
 
-def test_filter_by_currency(operations):
+def test_filter_by_currency_rub(operations):
     rub_currency = filter_by_currency(operations, "RUB")
     assert next(rub_currency)["id"] == 873106923
     assert next(rub_currency)["id"] == 594226727
@@ -32,13 +36,17 @@ def test_card_number_generator(start, end):
     assert next(card_number) == "0000 0000 0000 0004"
     assert next(card_number) == "0000 0000 0000 0005"
 
-@pytest.mark.parametrize('start, end, expected', [(1, 1, '0000 0000 0000 0001'),
-                                                  (2, 2, '0000 0000 0000 0002'),
-                                                  (3, 3, '0000 0000 0000 0003'),
-                                                  (4, 4, '0000 0000 0000 0004'),
-                                                  (5, 5, '0000 0000 0000 0005'),
-                                                  ]
-                         )
+
+@pytest.mark.parametrize(
+    "start, end, expected",
+    [
+        (1, 1, "0000 0000 0000 0001"),
+        (2, 2, "0000 0000 0000 0002"),
+        (3, 3, "0000 0000 0000 0003"),
+        (4, 4, "0000 0000 0000 0004"),
+        (5, 5, "0000 0000 0000 0005"),
+    ],
+)
 def test_card_number_generator(start, end, expected):
     card_number = card_number_generator(start, end)
     assert next(card_number) == expected
