128d450bedfe5903135ec3e98e2eb6558f3d7ba0
ludmila davletshina <ludaplv@mail.ru>
2024-05-30 12:57:52 +0300

    проверен black

--- src/decorators.py
+++ src/decorators.py
@@ -1,23 +1,25 @@
-
 def log(filename):
     def decorator(func):
         def wrapper(*args, **kwargs):
             try:
                 result = func(*args, **kwargs)
-                with open(filename, 'a') as file:
+                with open(filename, "a") as file:
                     file.write(f"{func.__name__} ok\n")
                     print(result)
             except Exception as e:
-                with open(filename, 'a') as file:
+                with open(filename, "a") as file:
                     file.write(f"{func.__name__}: ValueError. Inputs {args}, {kwargs}")
                     print(f"{func.__name__}: ValueError. Inputs {args}, {kwargs}")
             return result
+
         return wrapper
+
     return decorator
 
 
 @log(filename="mylog.txt")
 def my_function(x, y):
     return x + y
+
 
 my_function(1, 2)
