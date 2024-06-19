774218b88ef03bc58719c3b8293a51035fa11534
ludmila davletshina <ludaplv@mail.ru>
2024-06-19 14:06:33 +0300

    функции в файле transactions доработаны

--- src/transactions.py
+++ src/transactions.py
@@ -2,16 +2,19 @@ import csv
 
 import pandas as pd
 
-
 def read_transaction_csv(input_file_csv):
     """Считывание финансовых операций из csv файла"""
     with open(input_file_csv) as file:
-        reader = csv.reader(file, delimiter=";")
+        reader = csv.DictReader(file, delimiter=";")
+        next(reader)
+        new_list = []
         for row in reader:
-            print(row)
+            new_list.append(row)
+    return new_list
 
 
 def read_transaction_xlsx(input_file_xlsx):
-    """Считывание финансовых операций из xlsx файла"""
-    df = pd.read_excel(input_file_xlsx)
-    return df
+     """Считывание финансовых операций из xlsx файла"""
+     df = pd.read_excel(input_file_xlsx)
+     df_dict = df.to_dict(orient='records')
+     return df_dict
